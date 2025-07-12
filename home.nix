0de65db26b19d1ec9a23de91c38d3ac9ad8b9cff
neonguyen <nbi6731@gmail.com>
2025-07-12 20:39:13 +0700

    add google sdk & build-essential & move clangd to nix

--- home.nix
+++ home.nix
@@ -22,37 +22,36 @@
       enable = true;
 
       initContent = ''
-        zstyle ':completion:*' completer \
-          _expand _complete _match _approximate _correct _complete:-fuzzy _ignored
-        zstyle ':completion:*' special-dirs true
-        _comp_options+=(globdots)
+                zstyle ':completion:*' completer \
+                  _expand _complete _match _approximate _correct _complete:-fuzzy _ignored
+                zstyle ':completion:*' special-dirs true
+                _comp_options+=(globdots)
 
-        setopt globdots
-        # --- Plugin & Vi-Mode Loading BELOW THIS LINE ---
-        # e.g., znap, starship, direnv, etc.
+                setopt globdots
 
-        # --- After jeffreytse/zsh-vi-mode is sourced: ---
-        zvm_after_init_commands+=('bindkey -M viins "^N" menu-select')
-        zvm_after_init_commands+=('bindkey -M viins "^P" menu-select')
+                zvm_after_init_commands+=('bindkey -M viins "^N" menu-select')
+                zvm_after_init_commands+=('bindkey -M viins "^P" menu-select')
 
 
-        # Load Powerlevel10k theme
-        source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-        test -f ~/.p10k.zsh && source ~/.p10k.zsh
+                # Load Powerlevel10k theme
+                source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+                test -f ~/.p10k.zsh && source ~/.p10k.zsh
 
 
 
-        bindkey -M menuselect '\r' .accept-line
+                bindkey -M menuselect '\r' .accept-line
 
-        ci() {
-          if [ $# -eq 0 ]; then
-            echo "Usage: zn <pattern>"
-            return 1
-          fi
-          z "$@" && nvim .
-        }
+                ci() {
+                  if [ $# -eq 0 ]; then
+                    echo "Usage: zn <pattern>"
+                    return 1
+                  fi
+                  z "$@" && nvim .
+                }
 
-        macchina
+        eval "$(fnm env --use-on-cd --shell zsh)"
+
+                macchina
       '';
 
       completionInit = ''
@@ -74,9 +73,25 @@
         ll = "eza -lg --icons=always";
         la = "eza -lag --icons=always";
         lt = "eza -lTg --icons=always";
+
+        #Nvim
         nvm = "fnm";
 
+        #Nix
+        hms =
+          "home-manager switch --flake $HOME/.config/home-manager";
+
+        #Shine-wa
+        wadev =
+          "cloud-sql-proxy --address 0.0.0.0 --port 3306 one-global-ocps-dev:asia-southeast1:ocps-dev-db ";
+        waprod =
+          "cloud-sql-proxy --address 0.0.0.0 --port 3303 one-global-ocps-prod:asia-southeast1:ocps-prod-db2";
+        watest =
+          "cloud-sql-proxy --address 0.0.0.0 --port 3302 one-global-ocps-test:asia-southeast1:ocps-test-db";
+
       };
+
+      sessionVariables = { PATH = "$HOME/personal/work/WA:$PATH"; };
 
       antidote = {
         enable = true;
@@ -115,7 +130,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # nodejs
     zsh-powerlevel10k
     delta
     rustc
@@ -125,14 +139,23 @@
     zenBrowser
     macchina
     eza
-    gnumake
-    gcc
     vectorcode
     xclip
     python3
     bat
     flameshot
     fnm
+    google-cloud-sdk
+
+    gnumake
+    gcc
+    pkg-config
+    autoconf
+    automake
+    libtool
+    bison
+    flex
+    clang-tools
   ];
 
 }
