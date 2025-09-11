e00a63eecf1af7ce5f86dc2086c35a65b86fe183
neonguyen <nbi6731@gmail.com>
2025-09-11 16:50:50 +0700

    remove unused plugins for nvim-vm

--- nvim-vm/lua/kickstart/plugins/git-worktree.lua
+++
@@ -1,5 +0,0 @@
-return {
-  'anhbipro144/git-worktree.nvim',
-  -- config = function()
-  -- end
-}
--- nvim-vm/lua/lazy-plugins.lua
+++ nvim-vm/lua/lazy-plugins.lua
@@ -22,21 +22,7 @@ require('lazy').setup({
 
   require 'kickstart.plugins.cmp',
 
-  -- require 'kickstart.plugins.dap',
-
   require 'kickstart.plugins.barbar',
-
-  -- require 'kickstart.plugins.image',
-
-  -- require 'kickstart.plugins.auto-save',
-
-  -- require 'kickstart.plugins.notify',
-
-  -- require 'kickstart.plugins.img-clip',
-
-  -- require 'kickstart.plugins.markdown',
-
-  -- require 'kickstart.plugins.obsidian',
 
   require 'kickstart.plugins.persistence',
 
@@ -44,57 +30,19 @@ require('lazy').setup({
 
   require 'kickstart.plugins.incline',
 
-  -- require 'kickstart.plugins.toggleterm',
-
-  -- require 'kickstart.plugins.git-worktree',
-
   require 'kickstart.plugins.todo-comments',
 
-  -- require 'kickstart.plugins.outline',
-
-  -- require 'kickstart.plugins.neo-test',
-
   require 'kickstart.plugins.flash',
-
-  -- require 'kickstart.plugins.codecompanion',
-
-  -- require 'kickstart.plugins.copilotchat',
-
-  -- require 'kickstart.plugins.copilot',
-
-  -- require 'lua.kickstart.plugins.codecompanion-history',
-
-  -- require 'kickstart/plugins/avante',
-
-  -- require 'kickstart.plugins.prisma',
 
   require 'kickstart.plugins.lsp',
 
   require 'kickstart.plugins.lsp-preview',
 
-  -- require 'kickstart.plugins.autotag',
-
-  -- require 'kickstart.plugins.cursor-animate',
-
-  -- require 'kickstart/plugins/blink',
-
   require 'kickstart.plugins.snipe',
 
-  -- require 'kickstart.plugins.windows',
-
-  -- require 'kickstart.plugins.ts-tool',
-
-  -- require 'kickstart.plugins.noice',
-
-  -- require 'kickstart.plugins.comment',
+  require 'kickstart.plugins.comment',
 
   require 'kickstart.plugins.illuminate',
-
-  -- require 'kickstart.plugins.kulala',
-
-  -- require 'kickstart.plugins.vectorcode',
-
-  -- require 'kickstart/plugins/linting',
 
   require 'kickstart.plugins.mini',
 
